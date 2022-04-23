5206c2d99dddf049508113cc8bf7d0dc3e67e17c
sumon092 <m.sumon@outlook.com>
2022-04-23 16:36:50 +0600

    data sent to server

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const AddUser = () => {
+    const handleOnSubmit = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email };
+
+
+        //send data to server
+        fetch('', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+            })
+    }
+
+    return (
+        <div>
+            <h2>Please add user here</h2>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" name="name" placeholder='Name' id="" />
+                <br />
+                <input type="text" name="email" placeholder='Email' id="" />
+                <br />
+                <input type="submit" value="Add User" />
+
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
